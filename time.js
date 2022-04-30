068bb36faec5f51bc4d9e142e2debf43cc2a778a
Nestor Barone <jesuscaesar@outlook.com>
2022-04-30 12:44:50 +0000

    Add

--- time.js
+++ time.js
@@ -18,20 +18,20 @@ function clienttime() {
   var hour = now.getHours();
   var minute = now.getMinutes();
   var second = now.getSeconds();
-  if (hour.length > 1) {
+  if (hour.toString('').length > 1) {
     hours = '0' + hour;
   } else {
     hours = hour;
   }
-  if (minute.length > 1) {
+  if (minute.toString('').length > 1) {
     minutes = '0' + minute;
   } else {
     minutes = minute;
   }
-  if (second.length > 1) {
+  if (second.toString('').length > 1) {
     seconds = '0' + second;
   } else {
-    seconds = minute;
+    seconds = second;
   }
   var datetime = hours + ':' + minutes + ':' + seconds;
   document.getElementById('clienttime').innerText = datetime;
